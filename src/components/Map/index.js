56fe4b8afc0536ce58ceae53bf7a84def9c42b25
Rafael <rafael.acerqueirah@gmail.com>
2019-06-20 12:07:53 -0300

    Add React Modal and opens when click on the map

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,28 +1,70 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import ReactMapGL from 'react-map-gl'
+import Modal from 'react-modal'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
-const viewport = {
-	width: window.innerWidth,
-	height: window.innerHeight,
-	latitude: -10.9095,
-	longitude: -37.0748,
-	zoom: 14
-}
-
-const handleMapClick = e => {
-	console.log(e.lngLat)
+const customStyles = {
+	content: {
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-50%, -50%)'
+	}
 }
 
 const Map = () => {
+	const [viewport, setViewport] = useState({
+		width: '100vw',
+		height: '100vh',
+		latitude: -10.9095,
+		longitude: -37.0748,
+		zoom: 14
+	})
+
+	const [showModal, setShowModal] = useState(false)
+	const [repoName, setRepoName] = useState('')
+
+	const handleAddRepository = e => {
+		e.preventDefault()
+		console.log(repoName)
+	}
+
+	const handleMapClick = e => {
+		console.log(e.lngLat)
+		setShowModal(true)
+	}
+
 	return (
-		<ReactMapGL
-			{...viewport}
-			onClick={handleMapClick}
-			// onViewportChange={viewport => setViewport(viewport)}
-			mapboxApiAccessToken={process.env.MAPBOX_ACCESS_TOKEN}
-		/>
+		<Fragment>
+			<ReactMapGL
+				{...viewport}
+				onClick={handleMapClick}
+				onViewportChange={viewport => setViewport(viewport)}
+				mapboxApiAccessToken={process.env.MAPBOX_ACCESS_TOKEN}
+			/>
+			<Modal
+				isOpen={showModal}
+				onRequestClose={() => setShowModal(false)}
+				style={customStyles}
+				contentLabel="Example Modal"
+			>
+				<h2>Adicionar novo usuário</h2>
+				<form onSubmit={handleAddRepository}>
+					<input
+						type="text"
+						placeholder="Usuário do Github"
+						value={repoName}
+						onChange={e => setRepoName(e.target.value)}
+					/>
+					<button type="submit">Salvar</button>
+					<button onClick={() => setShowModal(false)}>Cancelar</button>
+				</form>
+			</Modal>
+		</Fragment>
 	)
 }
 
+Modal.setAppElement('#root')
 export default Map
