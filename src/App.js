2a739a00b00d4a4acf21da90efcafd06d5eef831
Rafael <rafael.acerqueirah@gmail.com>
2019-06-20 09:49:37 -0300

    Add and configure React Map GL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactMapGL from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
-const App = () => <div>React</div>
+const App = () => {
+	const [viewport, setViewport] = useState({
+		width: window.innerWidth,
+		height: window.innerHeight,
+		latitude: -10.9095,
+		longitude: -37.0748,
+		zoom: 14
+	})
 
+	const handleMapClick = e => {
+		console.log(e.lngLat)
+	}
+	return (
+		<ReactMapGL
+			{...viewport}
+			onClick={handleMapClick}
+			onViewportChange={viewport => setViewport(viewport)}
+			mapboxApiAccessToken={process.env.MAPBOX_ACCESS_TOKEN}
+		/>
+	)
+}
 export default App
