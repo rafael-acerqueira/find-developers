5d0887dbacb44cb1daec91c73fbf4b4b024f1ebf
Rafael <rafael.acerqueirah@gmail.com>
2019-06-21 10:07:07 -0300

    Add styled-components and global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import GlobalStyle from './styles/global'
 import './config/reactotron'
 import store from './store'
 import Map from './components/Map'
 
 const App = () => (
 	<Provider store={store}>
+		<GlobalStyle />
 		<Map />
 	</Provider>
 )
