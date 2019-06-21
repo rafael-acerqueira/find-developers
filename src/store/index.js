b9265d12f450368501d285eb83fdd06652ede834
Rafael <rafael.acerqueirah@gmail.com>
2019-06-21 10:40:27 -0300

    Install and configure Redux Saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,22 @@
 import { createStore, compose, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
 import reducers from './ducks'
+
+import sagas from './sagas'
+
+const middlewares = []
+
+const sagaMiddleware = createSagaMiddleware()
+
+middlewares.push(sagaMiddleware)
 
 const store = createStore(
 	reducers,
 	compose(
-		applyMiddleware(...[]),
+		applyMiddleware(...middlewares),
 		console.tron.createEnhancer()
 	)
 )
-
+sagaMiddleware.run(sagas)
 export default store
