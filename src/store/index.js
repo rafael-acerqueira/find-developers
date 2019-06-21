7ea459ffdffde24f04de52cb9f6dacaa378623ad
Rafael <rafael.acerqueirah@gmail.com>
2019-06-21 09:20:57 -0300

    Add reducers to Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import { createStore, compose, applyMiddleware } from 'redux'
 
+import reducers from './ducks'
+
 const store = createStore(
-	() => {},
+	reducers,
 	compose(
 		applyMiddleware(...[]),
 		console.tron.createEnhancer()
