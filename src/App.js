bd117aad0c699e2ef96bddb75782e5f90daeaed2
Rafael <rafael.acerqueirah@gmail.com>
2019-06-23 09:54:10 -0300

    Add toast when user is added successfully

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+
 import GlobalStyle from './styles/global'
 import './config/reactotron'
 import store from './store'
--- src/store/sagas/users.js
+++ src/store/sagas/users.js
@@ -1,9 +1,12 @@
 import { call, put } from 'redux-saga/effects'
+import { toast } from 'react-toastify'
 import api from '../../services/api'
 
 import { Creators as userActions } from '../ducks/users'
 import { Creators as modalActions } from '../ducks/modal'
 
+import 'react-toastify/dist/ReactToastify.css'
+toast.configure()
 export function * addUser (action) {
 	const { data } = yield call(api.get, `/users/${action.payload.name}`)
 
@@ -18,4 +21,7 @@ export function * addUser (action) {
 
 	yield put(userActions.addUserSuccess(userData))
 	yield put(modalActions.closemodal())
+	toast.success(`You successfully added ${userData.name}`, {
+		position: toast.POSITION.TOP_CENTER
+	})
 }
